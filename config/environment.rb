1d47e9bfe9a5cbbfcae003636dcd59a4b46044a4
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-18 17:44:28 -0500

    set up rotes, gemfile, and db yaml

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,5 @@
+
+#my_rails_root = File.expand_path('../..', __FILE__)
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 DBCHousingApp::Application.routes.draw do
+
+
+  resources :locations
+  resources :user
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -39,7 +43,7 @@ DBCHousingApp::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
