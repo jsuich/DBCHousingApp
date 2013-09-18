31d4c6605a835437a2e4c2dac7a7757e0653dd68
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-18 18:37:16 -0500

    removed git comments from code body

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,14 @@ DBCHousingApp::Application.routes.draw do
 
   resources :locations
   resources :user
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+
+
+  root :to => 'sessions#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
