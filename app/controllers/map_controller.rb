f0ef00b388fd995119b64ce9405c1cca5d58c7dc
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-19 17:11:34 -0500

    removed empty lines in codebase

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,18 +1,14 @@
 class MapController < ApplicationController
 
-def index
-  @locations = Location.all.to_a
+  def index
+    @locations = Location.all.to_a
+
+  end
 
 
+  def send_locations
 
-end
-
-
-def send_locations
-
-
-
-end
+  end
 
 
 end
