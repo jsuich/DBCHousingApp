9271d60e2b25b7ece1b0683aed36144e685406e1
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-19 15:22:39 -0500

    DBC omni-auth configurations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  DBC::token = "c013ce8202caae376838c037c9f60857"
+
+
 end
