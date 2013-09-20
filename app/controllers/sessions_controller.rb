5db799eff19b87ce635aa8e755efe10ef8efcfc8
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-20 11:35:39 -0500

    creating a user will route back to maps

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,18 +5,19 @@ class SessionsController < ApplicationController
   end
 
   def new
+    redirect_to '/auth/dbc'
 
   end
 
   def create
-    user_auth_attributes = request.env['omniauth.auth'].info
-    session[:user_attributes] = user_auth_attributes
-    user_full = DBC::User.find(session[:user_attributes][:id])
-    cohort_full = DBC::Cohort.find(user_full.cohort_id)
-    boot = User.find_or_create_by(email: session[:user_attributes].email)
-    boot.update_attributes(name: session[:user_attributes].name, cohort_name: cohort_full.name, cohort_id: user_full.cohort_id)
+    # user_auth_attributes = request.env['omniauth.auth'].info
+    # session[:user_attributes] = user_auth_attributes
+    # user_full = DBC::User.find(session[:user_attributes][:id])
+    # cohort_full = DBC::Cohort.find(user_full.cohort_id)
+    # boot = User.find_or_create_by(email: session[:user_attributes].email)
+    # boot.update_attributes(name: session[:user_attributes].name, cohort_name: cohort_full.name, cohort_id: user_full.cohort_id)
 
-    redirect_to '/map'
+    redirect_to root
   end
 
 
