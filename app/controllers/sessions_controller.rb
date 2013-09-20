68d39cdbb7f01f1ea77fb92779f99b2000990c45
Josh Suich <jsuich@gmail.com>
2013-09-20 09:58:04 -0500

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user_auth_attributes = request.env['omniauth.auth']#.info
+    user_auth_attributes = request.env['omniauth.auth'].info
     session[:user_attributes] = user_auth_attributes
     user_full = DBC::User.find(session[:user_attributes][:id])
     cohort_full = DBC::Cohort.find(user_full.cohort_id)
