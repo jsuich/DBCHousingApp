f9a03213dba98f45c7cce7282eda7668fcf87e6e
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-20 11:37:48 -0500

    saved extra file

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,14 +10,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # user_auth_attributes = request.env['omniauth.auth'].info
-    # session[:user_attributes] = user_auth_attributes
-    # user_full = DBC::User.find(session[:user_attributes][:id])
-    # cohort_full = DBC::Cohort.find(user_full.cohort_id)
-    # boot = User.find_or_create_by(email: session[:user_attributes].email)
-    # boot.update_attributes(name: session[:user_attributes].name, cohort_name: cohort_full.name, cohort_id: user_full.cohort_id)
+    user_auth_attributes = request.env['omniauth.auth'].info
+    session[:user_attributes] = user_auth_attributes
+    user_full = DBC::User.find(session[:user_attributes][:id])
+    cohort_full = DBC::Cohort.find(user_full.cohort_id)
+    boot = User.find_or_create_by(email: session[:user_attributes].email)
+    boot.update_attributes(name: session[:user_attributes].name, cohort_name: cohort_full.name, cohort_id: user_full.cohort_id)
 
-    redirect_to root
+    redirect_to '/map'
   end
 
 
