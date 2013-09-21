70933dc4a641173cc6193a576897925e8d35d067
nlprater <prater.nick@gmail.com>
2013-09-21 14:13:48 -0500

    added current_user to all routes, fixed location create to be 'find or create by', added name to location table, and twitter_handle and facebook_handle to user table

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,23 +1,31 @@
 class MapController < ApplicationController
 
   def index
-    @locations = Location.all.to_a
+    if current_user
+      @locations = Location.all.to_a
+    else
+      redirect_to '/'
+    end
   end
 
 
   def grablocations
-    empty_array = []
-    all_locations = Location.all
+    if current_user
+      empty_array = []
+      all_locations = Location.all
 
-    all_locations.each do |location|
-      empty_array << [buildHtml(location), (location.json_string)]
+      all_locations.each do |location|
+        empty_array << [buildHtml(location), (location.json_string)]
+      end
+      p empty_array
+
+      # all_locations = Location.all
+      # json_strings = []
+      # all_locations.each {|location| json_strings << location.json_string}
+      render :json => empty_array.to_json
+    else
+      redirect_to '/'
     end
-    p empty_array
-
-    # all_locations = Location.all
-    # json_strings = []
-    # all_locations.each {|location| json_strings << location.json_string}
-    render :json => empty_array.to_json
   end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,5 @@
 class SessionsController < ApplicationController
 
-  def index
-
-  end
-
   def new
 
   end
@@ -14,9 +10,8 @@ class SessionsController < ApplicationController
     user_full = DBC::User.find(session[:user_attributes][:id])
     cohort_full = DBC::Cohort.find(user_full.cohort_id)
     boot = User.find_or_create_by(email: session[:user_attributes].email)
-    boot.update_attributes(name: session[:user_attributes].name, cohort_name: cohort_full.name, gravatar: session[:user_attributes].gravatar, cohort_id: user_full.cohort_id)
+    boot.update_attributes(name: session[:user_attributes].name, cohort_name: cohort_full.name, gravatar: session[:user_attributes].gravatar, cohort_id: user_full.cohort_id,twitter_handle: user_full.profile[:twitter], facebook_handle: user_full.profile[:facebook])
     session[:user_id] = boot.id
-    redirect_to '/map'
   end
 
   def destroy
@@ -24,7 +19,4 @@ class SessionsController < ApplicationController
 
     redirect_to '/login'
   end
-
-
-
 end
