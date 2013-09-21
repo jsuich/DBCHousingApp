656ebc6f4f5a784604f3708fe8e81145f8a6b4dc
Josh Suich <jsuich@gmail.com>
2013-09-20 19:31:31 -0500

    uncommented config code

--- config/initializers/dbc.rb
+++ config/initializers/dbc.rb
@@ -1,6 +1,6 @@
-# env_config = YAML.load_file(Rails.root.join('config', 'dbc.yaml'))
+env_config = YAML.load_file(Rails.root.join('config', 'dbc.yaml'))
 
-# env_config.each do |key, value|
-#   ENV[key] = value
-# end
+env_config.each do |key, value|
+  ENV[key] = value
+end
 
