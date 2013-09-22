36dd49ec04188a3469fd423f1f049bceddfccdbe
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-21 19:35:04 -0500

    working filter

--- app/models/location.rb
+++ app/models/location.rb
@@ -5,4 +5,30 @@ class Location < ActiveRecord::Base
   validates :zip, presence: true
 
   has_many :users
+
+  def grab_cohorts
+
+    cohort = {}
+    cohorts = []
+    users = self.users
+    ids = []
+
+    users.each do |user|
+
+      cohorts.each do |cohort|
+        ids << cohort[:id]
+      end
+
+      unless ids.include?(user.cohort_id)
+        cohort[:id] = user.cohort_id
+        cohort[:name] = user.cohort_name.gsub(/\s.+/, '')
+        cohorts << cohort
+        cohort = {}
+      end
+        # cohort[user.cohort_id] = user.cohort_name.gsub(/\s.+/, '')
+    end
+
+    cohorts
+
+  end
 end
