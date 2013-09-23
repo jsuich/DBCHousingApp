d2c00bf2d1e4f3d0ff19741eeeb19d9f66f4c6e8
Josh Suich <jsuich@gmail.com>
2013-09-22 20:34:21 -0500

    commiting deletion of dbc.yaml

--- config/initializers/dbc.rb
+++ config/initializers/dbc.rb
@@ -1,4 +1,4 @@
-env_config = YAML.load_file(Rails.root.join('config', 'dbc.yaml'))
+env_config = YAML.load_file(Rails.root.join('config')) # for local development, re-include dbc.yaml after 'config' and put dbc.yaml back into rails directory
 
 env_config.each do |key, value|
   ENV[key] = value
