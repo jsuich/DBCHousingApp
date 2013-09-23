50586ddbc0f0429c30b33af34127845bb1d56b49
Josh Suich <jsuich@gmail.com>
2013-09-22 20:40:27 -0500

    Removing config db.rb

--- config/initializers/dbc.rb
+++
@@ -1,5 +0,0 @@
-env_config = YAML.load_file(Rails.root.join('config')) # for local development, re-include dbc.yaml after 'config' and put dbc.yaml back into rails directory
-
-env_config.each do |key, value|
-  ENV[key] = value
-end
